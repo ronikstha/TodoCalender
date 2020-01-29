423819d7d3229b9e174999e3ddb711b17e1ddd98
ronikstha <erosronik123@gmail.com>
2020-01-29 15:47:32 +1100

    app module file fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { SignupPageModule } from './signup/signup.module';
+import { AddPageModule } from './add/add.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp( environment.firebase ),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    SignupPageModule,
+    AddPageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
