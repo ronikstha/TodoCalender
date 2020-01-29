c416a62d5a404ae7e7f708a5af5663e8f04da0fc
ronikstha <erosronik123@gmail.com>
2020-01-29 13:29:00 +1100

    Pages added and errors fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCBqX1CB3Y70wPnl4rUWutm0IVObUZ3V50",
+    authDomain: "todo-calender-ionic.firebaseapp.com",
+    databaseURL: "https://todo-calender-ionic.firebaseio.com",
+    projectId: "todo-calender-ionic",
+    storageBucket: "todo-calender-ionic.appspot.com",
+    messagingSenderId: "830208841188",
+    appId: "1:830208841188:web:b28c2d071756a3b589e3f1",
+    measurementId: "G-252WM40T7P"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCBqX1CB3Y70wPnl4rUWutm0IVObUZ3V50",
+    authDomain: "todo-calender-ionic.firebaseapp.com",
+    databaseURL: "https://todo-calender-ionic.firebaseio.com",
+    projectId: "todo-calender-ionic",
+    storageBucket: "todo-calender-ionic.appspot.com",
+    messagingSenderId: "830208841188",
+    appId: "1:830208841188:web:b28c2d071756a3b589e3f1",
+    measurementId: "G-252WM40T7P"
+  }
 };
 
 /*
